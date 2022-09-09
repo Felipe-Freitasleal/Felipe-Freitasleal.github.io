d9ca4307cbc366b4113c747f45bb005bec1980d7
Felipe-Freitasleal <felipeleal094@gmail.com>
2022-09-09 12:02:57 -0300

    Modificações semana 2

--- projeto-intro-web.js
+++ projeto-intro-web.js
@@ -1,43 +1,35 @@
-//Lista Pokémons
+//Semana 1: cria uma lista de ítens: criei uma lista de Pokémons.
+//Semana 2: transformar os ítens em objeto. Após isso, adicionar os que tem valor da chave booleana true numa array, mas como o valor booleano que atribuí aos objetos pokemons foi a verificação se são básicos ou não e só entra nessa array se o valor do booleano fo true, denominei esa array de "soPokemonBasico" e apenas os que tiverem valor true na chave "basico" entram no array, caso não, aciona o alert para o usuário. Usei o if ternário para avaliar o valor booleano de cada ítem "bulbasaur.basico".
+const bulbasaur ={
+    nome: "Bulbasaur",
+    pokenumero: 1,
+    basico: true,
+    desvantagem: ["fogo", "psíquico", "voador", "gelo"]
+}
+const scyther = {
+    nome: "Scyther",
+    pokenumero: 123,
+    basico: true,
+    desvantagem: ["fogo", "voador", "elétrico", "gelo", "pedra"]
+}
+const gyarados ={
+    nome: "Gyrados",
+    pokenumero: 130,
+    basico: false,
+    desvantagem: ["eletrico", "pedra"]
+}
 
-const nome1 = "Bulbasaur"
-const pokenumero1 = 1
-const Basico1 = True 
-const desvantagem1 = ["fogo", "psíquico", "voador", "gelo"]
-
-const nome2 = "Scyther"
-const pokenumero2 = 123
-const Basico2 = True 
-const desvantagem2 = ["fogo", "voador", "elétrico", "gelo", "pedra"]
-
-const nome3 = "Gyarados"
-const pokenumero3 = 130
-const Basico3 = False 
-const desvantagem3 = ["eletrico", "pedra"]
-
-const mediaPokenumero = (pokenumero1 + pokenumero2 + pokenumero3 / 3)
+//Semana 2
+let soPokemonbasico = []
+bulbasaur.basico?soPokemonbasico.push(bulbasaur):alert("Bulbasaur não é um pokemon básico, por favor, insira um pokemon básico.")
+scyther.basico?soPokemonbasico.push(scyther):alert("Scyther não é um pokemon básico, por favor, insira um pokemon básico.")
+gyarados.basico?soPokemonbasico.push(gyarados):alert("Gyrados não é um pokemon básico, por favor, insira um pokemon básico.")
+console.log (soPokemonbasico)
+//Semana 1
+const mediaPokenumero = bulbasaur.pokenumero + scyther.pokenumero +gyarados.pokenumero / 3
 console.log (mediaPokenumero)
-
-const Basicos = Basico1 && Basico2 && Basico3
-console.log (Basicos)
-
-console.log (`
-${nome1.toUpperCase()}
-Pokenúmero : ${pokenumero1}
-É básico? ${Basico1}
-Desvantagem: ${desvantagem1}
-`)
-
-console.log (`
-${nome2.toUpperCase()}
-Pokenúmero : ${pokenumero2}
-É básico? ${Basico2}
-Desvantagem: ${desvantagem2}
-`)
-
-console.log (`
-${nome3.toUpperCase()}
-Pokenúmero : ${pokenumero3}
-É básico? ${Basico3}
-Desvantagem: ${desvantagem3}
-`)
+const basicos = bulbasaur.basico && scyther.basico && gyarados.basico
+console.log (basicos)
+console.log (bulbasaur)
+console.log (scyther)
+console.log (gyarados)
